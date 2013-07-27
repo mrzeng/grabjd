268d56ebd1dca9547fc7989c5be0e6d22615b4fc
czbabl <czbabl@vip.qq.com>
2013-07-27 08:27:26 +0800

    修改sql

--- doc/no2.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE g_goods;
-CREATE TABLE g_goods (id bigint NOT NULL AUTO_INCREMENT, title varchar(200), cost_price bigint, discount_price bigint, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-DROP TABLE g_link;
-CREATE TABLE g_link (id bigint NOT NULL AUTO_INCREMENT, link_name varchar(100), link_url varchar(200), period bigint, etime bigint, status tinyint, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
